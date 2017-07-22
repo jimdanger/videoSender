35d69855b0520f7c9447afc1a27834503e837096
Jim Wilson <jamesmattheywilson@gmail.com>
2017-07-22 09:52:31 -0400

    Initial Commit

---
+++ VideoSender/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoSender
+//
+//  Created by James Wilson on 7/22/17.
+//  Copyright © 2017 jimdanger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoSender/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoSender
+//
+//  Created by James Wilson on 7/22/17.
+//  Copyright © 2017 jimdanger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
